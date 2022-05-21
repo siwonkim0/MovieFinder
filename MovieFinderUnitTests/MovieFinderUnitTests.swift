e92c1dae5243b9338a81f1407aaf18fedfb2d9f4
siwonkim0 <siyeon0616@naver.com>
2022-05-21 14:52:34 +0900

    test. APIManager Unit Test #4
    - get, post, delete 메서드 테스트 코드 작성

---
+++ MovieFinderUnitTests/MovieFinderUnitTests.swift
@@ -0,0 +1,86 @@
+//
+//  MovieFinderUnitTests.swift
+//  MovieFinderUnitTests
+//
+//  Created by Siwon Kim on 2022/05/21.
+//
+
+import XCTest
+@testable import MovieFinder
+
+class MovieFinderUnitTests: XCTestCase {
+    var sut: APIManager!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = APIManager()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    func test_GetApiCallGetsHTTPStatusCode200() throws {
+        let url = MovieURL.token.url
+        let promise = expectation(description: "Status code: 200")
+        
+        sut.getData(from: url, format: Token.self) { result in
+            switch result {
+            case .success(_):
+                promise.fulfill()
+            case .failure(let error):
+                XCTFail("\(error)")
+            }
+        }
+        wait(for: [promise], timeout: 1)
+    }
+    
+    func test_GetImageApiCallGetsHTTPStatusCode200() throws {
+        let url = MovieURL.image(posterPath: "2tOgiY533JSFp7OrVlkeRJvsZpI.jpg").url
+        let promise = expectation(description: "Status code: 200")
+        
+        sut.getImage(with: url) { result in
+            switch result {
+            case .success(_):
+                promise.fulfill()
+            case .failure(let error):
+                XCTFail("\(error)")
+            }
+        }
+        wait(for: [promise], timeout: 1)
+    }
+    
+    func test_PostApiCallGetsHTTPStatusCode200() throws {
+        let jsonData = JSONParser.encodeToData(with: Rate(value: 4.5))
+        let sessionID = KeychainManager.shared.getSessionID()
+        let url = MovieURL.rateMovie(sessionID: sessionID, movieID: 284052).url
+        let promise = expectation(description: "Status code: 200")
+        
+        sut.postData(jsonData, to: url) { result in
+            switch result {
+            case .success(_):
+                promise.fulfill()
+            case .failure(let error):
+                XCTFail("\(error)")
+            }
+        }
+        wait(for: [promise], timeout: 1)
+    }
+    
+    func test_DeleteApiCallGetsHTTPStatusCode200() throws {
+        let sessionID = KeychainManager.shared.getSessionID()
+        let url = MovieURL.deleteRating(sessionID: sessionID, movieID: 284052).url
+        let promise = expectation(description: "Status code: 200")
+        
+        sut.deleteData(at: url) { result in
+            switch result {
+            case .success(_):
+                promise.fulfill()
+            case .failure(let error):
+                XCTFail("\(error)")
+            }
+        }
+        wait(for: [promise], timeout: 1)
+    }
+}
