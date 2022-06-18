8d96a6394ce3eb376d7eea322a6cbd5ef34ba12e
siwonkim0 <siyeon0616@naver.com>
2022-06-18 23:52:12 +0900

    chore: 쓸모없는 코드 삭제

--- MovieFinderUnitTests/Test Double/FakeDefaultMoviesUseCase.swift
+++ MovieFinderUnitTests/Test Double/FakeDefaultMoviesUseCase.swift
@@ -11,28 +11,10 @@ import RxSwift
 
 class FakeDefaultMoviesUseCase: MoviesUseCase {
     func getMovieListItem(from listUrl: MovieListURL) -> Observable<[MovieListItem]> {
-        switch listUrl {
-        case .nowPlaying:
-            return Observable.create { observer in
-                observer.onNext([MovieListItem(id: 338953, title: "Fantastic Beasts: The Secrets of Dumbledore", overview: "Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.", releaseDate: "2022-04-06", posterPath: "/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg", originalLanguage: .english, genres: [Genre(id: 28, name: "Adventure")])])
-                return Disposables.create()
-            }
-            
-        case .popular:
-            return Observable.create { observer in
-                observer.onNext([MovieListItem(id: 338953, title: "Fantastic Beasts: The Secrets of Dumbledore", overview: "Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.", releaseDate: "2022-04-06", posterPath: "/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg", originalLanguage: .english, genres: [Genre(id: 28, name: "Adventure")])])
-                return Disposables.create()
-            }
-        case .topRated:
-            return Observable.create { observer in
-                observer.onNext([MovieListItem(id: 338953, title: "Fantastic Beasts: The Secrets of Dumbledore", overview: "Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.", releaseDate: "2022-04-06", posterPath: "/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg", originalLanguage: .english, genres: [Genre(id: 28, name: "Adventure")])])
-                return Disposables.create()
-            }
-        case .upComing:
-            return Observable.create { observer in
-                observer.onNext([MovieListItem(id: 338953, title: "Fantastic Beasts: The Secrets of Dumbledore", overview: "Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.", releaseDate: "2022-04-06", posterPath: "/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg", originalLanguage: .english, genres: [Genre(id: 28, name: "Adventure")])])
-                return Disposables.create()
-            }
+        return Observable.create { observer in
+            observer.onNext([MovieListItem(id: 338953, title: "Fantastic Beasts: The Secrets of Dumbledore", overview: "Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.", releaseDate: "2022-04-06", posterPath: "/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg", originalLanguage: .english, genres: [Genre(id: 28, name: "Adventure")]),
+                             MovieListItem(id: 338953, title: "Fantastic Beasts: The Secrets of Dumbledore", overview: "Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.", releaseDate: "2022-04-06", posterPath: "/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg", originalLanguage: .english, genres: [Genre(id: 28, name: "Adventure")])])
+            return Disposables.create()
         }
     }
 }
