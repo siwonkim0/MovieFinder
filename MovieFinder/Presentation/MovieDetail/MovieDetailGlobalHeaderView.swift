9d25dbd9e62c0b8efa65a351c63f1761fa5c3dc1
siwonkim0 <siyeon0616@naver.com>
2022-06-20 18:22:06 +0900

    feat: MovieDetailGlobalHeaderView XIB 생성 #12

---
+++ MovieFinder/Presentation/MovieDetail/MovieDetailGlobalHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailGlobalHeaderView.swift
+//  MovieFinder
+//
+//  Created by Siwon Kim on 2022/06/20.
+//
+
+import UIKit
+
+class MovieDetailGlobalHeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
