94f94d44fd018004c57edf33c422748c4e068c90
Siwon Kim <siyeon0616@naver.com>
2022-10-14 23:04:42 +0900

    test: SearchViewReactor 테스트 구현

---
+++ MovieFinderUnitTests/SearchViewReactorTests.swift
@@ -0,0 +1,47 @@
+//
+//  SearchViewReactorTests.swift
+//  MovieFinderUnitTests
+//
+//  Created by Siwon Kim on 2022/10/14.
+//
+
+import XCTest
+@testable import MovieFinder
+
+import RxSwift
+
+final class SearchViewReactorTests: XCTestCase {
+    var reactor: SearchViewReactor!
+    var spyUseCase: SpyDefaultMoviesUseCase!
+    
+    override func setUp() {
+        spyUseCase = SpyDefaultMoviesUseCase()
+        reactor = SearchViewReactor(useCase: spyUseCase)
+    }
+    
+    func test_setKeyword() {
+        let keyword = "test"
+        reactor.action.onNext(.searchKeyword(keyword))
+        
+        XCTAssertEqual(reactor.currentState.keyword, "test")
+        XCTAssertEqual(reactor.currentState.movieResults.count, 2)
+        spyUseCase.verifyGetSearchResults(callCount: 1)
+    }
+    
+    func test_loadNextPage() {
+        XCTAssertEqual(reactor.currentState.nextPage, 1)
+        
+        reactor.action.onNext(.loadNextPage)
+        XCTAssertEqual(reactor.currentState.nextPage, 2)
+        XCTAssertEqual(reactor.currentState.isLoadingNextPage, false)
+    }
+    
+    func test_clearSearchKeyword() {
+        reactor.initialState.keyword = "ask"
+        
+        reactor.action.onNext(.clearSearchKeyword)
+        XCTAssertEqual(reactor.currentState.keyword, "")
+        XCTAssertEqual(reactor.currentState.movieResults, [])
+    }
+    
+}
